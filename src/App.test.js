650444ded06ce0e9b052afa52b7f93eb87aa592f
Matthew Benjamin Schupack <mschupa@c02xr4e7jgh6.us.aexp.com>
2020-07-30 16:51:12 -0400

    make tests fail

--- src/App.test.js
+++ src/App.test.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
 
 it('renders the right thing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
-  expect(div.innerHTML).toMatchSnapshot();
+  expect(div.innerHTML).not.toMatchSnapshot();
 });
